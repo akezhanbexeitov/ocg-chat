56e947927f476843605886b7135a14c3ac4062d1
Akezhan <akidulatini@gmail.com>
2024-02-17 17:19:02 +0600

    Create chat header

--- src/app/[chatId]/page.tsx
+++ src/app/[chatId]/page.tsx
@@ -1,11 +1,12 @@
+import ChatHeader from "@/components/ChatHeader";
 import styles from "./page.module.scss";
 
 export default function Page() {
   return (
     <div className={styles.chat}>
-      <div className={styles["chat__header"]}>Chat header</div>
-      <div className={styles["chat__body"]}>Chat body</div>
-      <div className={styles["chat__footer"]}>Chat footer</div>
+      <ChatHeader className={styles.chat__header} />
+      <div className={styles.chat__body}>Chat body</div>
+      <div className={styles.chat__footer}>Chat footer</div>
     </div>
   );
 }
---
+++ src/components/ChatHeader/index.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./index.module.scss";
+import classNames from "classnames";
+import Ellipsis from "../icons/Ellipsis";
+
+interface Props {
+  className?: string;
+}
+
+const cn = classNames.bind(styles);
+
+export default function ChatHeader({ className }: Props) {
+  return (
+    <header className={cn(className, styles["chat-header"])}>
+      <div className={styles["chat-header__left"]}>
+        <div className={styles["chat-header__avatar"]}>
+          <Image
+            src="/images/avatar.png"
+            width={50}
+            height={50}
+            alt="Chat avatar"
+          />
+        </div>
+        <h1 className={styles["chat-header__title"]}>Chat title</h1>
+      </div>
+      <div className={styles["chat-header__right"]}>
+        <button>
+          <Ellipsis />
+        </button>
+      </div>
+    </header>
+  );
+}
